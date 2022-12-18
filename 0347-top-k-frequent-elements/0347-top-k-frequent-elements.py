b0e42a00646bf9c78fab98774c96cbaa2f68d104
이민진 <54443194+lmj00@users.noreply.github.com>
2022-12-19 00:20:34 +0900

    Time: 198 ms (46.73%), Space: 16.8 MB (59.78%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        ls = [i[0] for i in Counter(nums).most_common(k)]
+        
+        return ls
