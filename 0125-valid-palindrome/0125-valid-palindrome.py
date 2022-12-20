a7f95416fd2c6406ff8961f9c546134e33627242
이민진 <54443194+lmj00@users.noreply.github.com>
2022-12-21 03:41:26 +0900

    Time: 60 ms (77.84%), Space: 14.8 MB (38.84%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,12 +5,5 @@ class Solution:
         for i in s.lower():
             if 97 <= ord(i) <= 122 or 48 <= ord(i) <= 57:
                 ss += i
-
         
-        i = 0
-        for j in range(len(ss) - 1, len(ss) // 2 - 1, -1):
-            if ss[i] != ss[j]:
-                return False
-            i += 1
-            
-        return True
+        return ss == ss[::-1]
