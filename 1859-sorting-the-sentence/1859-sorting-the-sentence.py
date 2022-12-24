fd8f90f850506d7b3708ff9922397ddf987de310
이민진 <54443194+lmj00@users.noreply.github.com>
2022-12-25 04:42:42 +0900

    Time: 32 ms (88.77%), Space: 13.8 MB (57.47%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -4,10 +4,9 @@ class Solution:
     def sortSentence(self, s: str) -> str:
          
         s = sorted(s.split(), key=lambda x:x[-1])
+        v = ''
         
         for i in range(len(s)):
-            s[i] = s[i].replace(s[i][-1], '') 
-
-        result = ' '.join(map(str, s))
-
-        return result
+            v += s[i].replace(s[i][-1], ' ') 
+        
+        return v.rstrip()
