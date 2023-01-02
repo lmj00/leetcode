46818615900147354340f69fe7883b18d32116cb
이민진 <54443194+lmj00@users.noreply.github.com>
2023-01-02 22:56:25 +0900

    Time: 66 ms (67.56%), Space: 13.9 MB (14.64%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.py
@@ -0,0 +1,9 @@
+class Solution:
+    def restoreString(self, s: str, indices: List[int]) -> str: 
+        
+        ss = ''
+        
+        for i in range(len(s)):
+            ss += s[indices.index(i)]
+            
+        return ss
